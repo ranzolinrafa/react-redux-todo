0305359b88c18d095824a37518dd2dc6d851f2c9
Rafael Ranzolin Pedroso <ranzolin.rafa@gmail.com>
2020-04-02 00:44:01 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My App</h1>
+    </div>
+  );
+}
+
+export default App;
